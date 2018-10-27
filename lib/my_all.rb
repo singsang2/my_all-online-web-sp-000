3e78397c97f2aa753e015f53ef7a5a6826de5a01
Sung Bae <singsang2@gmail.com>
2018-10-27 23:37:17 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,6 +5,7 @@ def my_all?(collection)
   collect = []
   while counter < collection.length
     collect << yield(collection[counter])
+    counter += 1
   end
   collect.include?(false) ? false : true
 end
