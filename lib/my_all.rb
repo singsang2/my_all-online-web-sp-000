3ae059126426c07670bb7df496c2ca731e696dc0
Sung Bae <singsang2@gmail.com>
2018-10-27 23:36:12 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  collect = []
+  while counter < collection.length
+    collect << yield(collection[counter])
+  end
+  collect.include?(false) ? false : true
 end
